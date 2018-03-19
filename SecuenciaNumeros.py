d408d30f4e4376a5666b5265e61b735858d7cd87
Sergio Perez <se.perezf@uniandes.edu.co>
2018-03-18 19:44:21 -0500

    Paso 21 - Se realizan optimizaciones en reutilizacion de codigo (Verde)

--- SecuenciaNumeros.py
+++ SecuenciaNumeros.py
@@ -2,42 +2,30 @@ __author__ = 'af.carrion, se.perezf'
 
 class SecuenciaNumeros:
 
+    def a_arreglo(self, cadena):
+        #funtion map: es "equivalente" a for x in cadena
+        return map(float, cadena.split(","))
+
     def longitud(self,cadena):
         if cadena == "":
             return 0
-        elif "," in cadena:
-            return len(cadena.split(","))
-        else:
-            return 1
+        return len(self.a_arreglo(cadena))
 
     def minimo(self, cadena):
         if cadena == "":
             return 0
-        elif "," in cadena:
-            #funtion map: es equivalente a for x in cadena
-            minimo = map(int, cadena.split(","))
-            #function min It returns the smallest item in an iterable or the smallest of two or more arguments.
-            return min(minimo)
-        else:
-            return int(cadena)
+        return min(self.a_arreglo(cadena))
 
     def maximo(self,cadena):
         if cadena == "":
             return 0
-        elif "," in cadena:
-            maximo_arreglo = map(int, cadena.split(","))
-            return max(maximo_arreglo)
-        else:
-            return int(cadena)
+        return max(self.a_arreglo(cadena))
 
     def promedio(self, cadena):
         if cadena == "":
             return 0
-        elif "," in cadena:
-            promedio_arreglo = map(int, cadena.split(","))
-            return sum(promedio_arreglo)/float(len(promedio_arreglo))
-        else:
-            return int(cadena)
+        promedio_arreglo = self.a_arreglo(cadena)
+        return sum(promedio_arreglo)/len(promedio_arreglo)
 
     def analizar(self, cadena):
         return [self.longitud(cadena), self.minimo(cadena), self.maximo(cadena), self.promedio(cadena)]
